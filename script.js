41ca4b869532dba903bd45fca065375844142c13
Ilia Bochkov <ilya.bochkov97@gmail.com>
2022-12-20 00:13:50 +0200

    Improve functionality of existing logic

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const Player = sign => {
     const returnSign = () => {
         return sign;
     }
-    return {returnSign, sign};
+    return {returnSign};
 }
 
 const GameBoard = (() => {
@@ -30,12 +30,22 @@ const ScreenController = (() => {
         display.innerHTML = `Player ${sign}'s turn`
     }
 
+    const updateDisplayWictory = (sign) => {
+        display.innerHTML = `Player ${sign} won!<br />
+        Press Restart to start again!`
+    }
+    const updateDisplayDraw = () => {
+        display.innerHTML = `It's a draw!`
+    }
+
     const listenForClick = () => {
         cells.forEach(cell => {
             cell.addEventListener("click", e => {
                 latestIndex = cell.getAttribute("data-index");
-                cell.innerHTML = GameController.returnCurrentPlayer().returnSign();
-                GameController.PlayRound(latestIndex);
+                if (!cell.innerHTML & !GameController.returnWictory() & !GameController.checkForDraw()) {
+                    cell.innerHTML = GameController.returnCurrentPlayer().returnSign();
+                    GameController.PlayRound(latestIndex);
+                }
             })
         })
     }
@@ -44,7 +54,7 @@ const ScreenController = (() => {
         return latestIndex;
     }
 
-    return {updateDisplay, listenForClick, returnLatestIndex};
+    return {updateDisplay, listenForClick, returnLatestIndex, updateDisplayWictory, updateDisplayDraw};
 })()
 
 const GameController = (() => {
@@ -52,155 +62,60 @@ const GameController = (() => {
     const playerO = Player("O");
     let currentPlayer = playerX;
     ScreenController.listenForClick();
+    let wictory = false;
 
     const PlayRound = (index) => {
-        if (currentPlayer == playerX) currentPlayer = playerO;
-        else currentPlayer = playerX;
-        ScreenController.updateDisplay(currentPlayer.returnSign());
+        GameBoard.addSign(index, currentPlayer.returnSign())
+        if (checkForWin(currentPlayer.returnSign())) {
+            ScreenController.updateDisplayWictory(currentPlayer.returnSign());
+            wictory = true;
+            return;
+        } 
+        else if (GameController.checkForDraw()) {
+            ScreenController.updateDisplayDraw();
+            return;
+        }
+        else {
+            if (currentPlayer == playerX) currentPlayer = playerO;
+            else currentPlayer = playerX;
+            ScreenController.updateDisplay(currentPlayer.returnSign());
+            return
+        }
     }
 
     const returnCurrentPlayer = () => {
         return currentPlayer;
     }
+
+    const returnWictory = () => {
+        return wictory;
+    }
+
+
+    const checkForWin = (sign) => {
+        let condition = false;
+        const board = GameBoard.returnBoard();
+        if (board[0]===sign & board[1]===sign & board[2]===sign) condition = true;
+        if (board[3]===sign & board[4]===sign & board[5]===sign) condition = true;
+        if (board[6]===sign & board[7]===sign & board[8]===sign) condition = true;
+        
+        if (board[0]===sign & board[3]===sign & board[6]===sign) condition = true;
+        if (board[1]===sign & board[4]===sign & board[7]===sign) condition = true;
+        if (board[2]===sign & board[5]===sign & board[8]===sign) condition = true;
+        
+        if (board[0]===sign & board[4]===sign & board[8]===sign) condition = true;
+        if (board[2]===sign & board[4]===sign & board[6]===sign) condition = true;
+        // console.log(condition)
+        return condition;
+    }
+
+    const checkForDraw = () => {
+        const board = GameBoard.returnBoard();
+        for (const i in board) {
+            if (!board[i]) return false;
+        }
+        return true
+    }
  
-    return {returnCurrentPlayer, PlayRound};
+    return {returnCurrentPlayer, PlayRound, checkForWin, checkForDraw, returnWictory};
 })()
-
-
-
-
-// "use strict";
-
-// const Player = (sign) => {
-//   this.sign = sign;
-
-//   const getSign = () => {
-//     return sign;
-//   };
-
-//   return { getSign };
-// };
-
-// const gameBoard = (() => {
-//   const board = ["", "", "", "", "", "", "", "", ""];
-
-//   const setField = (index, sign) => {
-//     if (index > board.length) return;
-//     board[index] = sign;
-//   };
-
-//   const getField = (index) => {
-//     if (index > board.length) return;
-//     return board[index];
-//   };
-
-//   const reset = () => {
-//     for (let i = 0; i < board.length; i++) {
-//       board[i] = "";
-//     }
-//   };
-
-//   return { setField, getField, reset };
-// })();
-
-// const displayController = (() => {
-//   const fieldElements = document.querySelectorAll(".field");
-//   const messageElement = document.getElementById("message");
-//   const restartButton = document.getElementById("restart-button");
-
-//   fieldElements.forEach((field) =>
-//     field.addEventListener("click", (e) => {
-//       if (gameController.getIsOver() || e.target.textContent !== "") return;
-//       gameController.playRound(parseInt(e.target.dataset.index));
-//       updateGameboard();
-//     })
-//   );
-
-//   restartButton.addEventListener("click", (e) => {
-//     gameBoard.reset();
-//     gameController.reset();
-//     updateGameboard();
-//     setMessageElement("Player X's turn");
-//   });
-
-//   const updateGameboard = () => {
-//     for (let i = 0; i < fieldElements.length; i++) {
-//       fieldElements[i].textContent = gameBoard.getField(i);
-//     }
-//   };
-
-//   const setResultMessage = (winner) => {
-//     if (winner === "Draw") {
-//       setMessageElement("It's a draw!");
-//     } else {
-//       setMessageElement(`Player ${winner} has won!`);
-//     }
-//   };
-
-//   const setMessageElement = (message) => {
-//     messageElement.textContent = message;
-//   };
-
-//   return { setResultMessage, setMessageElement };
-// })();
-
-// const gameController = (() => {
-//   const playerX = Player("X");
-//   const playerO = Player("O");
-//   let round = 1;
-//   let isOver = false;
-
-//   const playRound = (fieldIndex) => {
-//     gameBoard.setField(fieldIndex, getCurrentPlayerSign());
-//     if (checkWinner(fieldIndex)) {
-//       displayController.setResultMessage(getCurrentPlayerSign());
-//       isOver = true;
-//       return;
-//     }
-//     if (round === 9) {
-//       displayController.setResultMessage("Draw");
-//       isOver = true;
-//       return;
-//     }
-//     round++;
-//     displayController.setMessageElement(
-//       `Player ${getCurrentPlayerSign()}'s turn`
-//     );
-//   };
-
-//   const getCurrentPlayerSign = () => {
-//     return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
-//   };
-
-//   const checkWinner = (fieldIndex) => {
-//     const winConditions = [
-//       [0, 1, 2],
-//       [3, 4, 5],
-//       [6, 7, 8],
-//       [0, 3, 6],
-//       [1, 4, 7],
-//       [2, 5, 8],
-//       [0, 4, 8],
-//       [2, 4, 6],
-//     ];
-
-//     return winConditions
-//       .filter((combination) => combination.includes(fieldIndex))
-//       .some((possibleCombination) =>
-//         possibleCombination.every(
-//           (index) => gameBoard.getField(index) === getCurrentPlayerSign()
-//         )
-//       );
-//   };
-
-//   const getIsOver = () => {
-//     return isOver;
-//   };
-
-//   const reset = () => {
-//     round = 1;
-//     isOver = false;
-//   };
-
-//   return { playRound, getIsOver, reset };
-// })();
